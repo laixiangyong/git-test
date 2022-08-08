f961a1222caec04464c1b78edc24db8cd8e328e5
laixy <laixy@lai.com>
2022-08-08 11:13:17 +0800

    third commit

--- src/main/java/com/lia/git/GitTest.java
+++ src/main/java/com/lia/git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello 鸡哥!");
+        System.out.println("hello 你干嘛!");
     }
 
 }
