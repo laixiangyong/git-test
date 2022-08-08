39e1b550190b433643c09a1efa5e84da0bbacf18
laixy <laixy@lai.com>
2022-08-08 13:57:21 +0800

    hot-fix first commit

--- src/main/java/com/lia/git/GitTest.java
+++ src/main/java/com/lia/git/GitTest.java
@@ -6,6 +6,8 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello 鸡哥!");
         System.out.println("hello 你干嘛!");
+        System.out.println("纯路人，没必要（流汗黄豆）");
+        System.out.println("小黑子，什么意思？");
     }
 
 }
